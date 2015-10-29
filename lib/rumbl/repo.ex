60fa043e62fe6d59cb118e9ccfe0f7b56d2eb580
Nathan Wise <nathanwise@gmail.com>
2015-10-29 08:13:27 -0400

    Adding UserController

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,3 +1,22 @@
 defmodule Rumbl.Repo do
-  use Ecto.Repo, otp_app: :rumbl
+  @moduledoc """
+  In memory repository.
+  """
+
+  def all(Rumbl.User) do
+    [%Rumbl.User{id: "1", name: "Nate Wise", username: "nwise", password: "abcd1234"},
+      %Rumbl.User{id: "2", name: "John Doe", username: "jdoe", password: "abcd1234"},
+      %Rumbl.User{id: "3", name: "fubar", username: "fbar", password: "abcd1234"}]
+  end
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
 end
