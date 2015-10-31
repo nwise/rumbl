fb748c41dc420d2993aae05754a3e5db3df3b2a6
Nathan Wise <nathanwise@gmail.com>
2015-10-31 11:48:49 -0400

    Adding User Model wired up to the DB and web form.

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,22 +1,19 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In memory repository.
-  """
+  use Ecto.Repo, otp_app: :rumbl
+  #def all(Rumbl.User) do
+    #[%Rumbl.User{id: "1", name: "Nate Wise", username: "nwise", password: "abcd1234"},
+      #%Rumbl.User{id: "2", name: "John Doe", username: "jdoe", password: "abcd1234"},
+      #%Rumbl.User{id: "3", name: "fubar", username: "fbar", password: "abcd1234"}]
+  #end
+  #def all(_module), do: []
 
-  def all(Rumbl.User) do
-    [%Rumbl.User{id: "1", name: "Nate Wise", username: "nwise", password: "abcd1234"},
-      %Rumbl.User{id: "2", name: "John Doe", username: "jdoe", password: "abcd1234"},
-      %Rumbl.User{id: "3", name: "fubar", username: "fbar", password: "abcd1234"}]
-  end
-  def all(_module), do: []
+  #def get(module, id) do
+    #Enum.find all(module), fn map -> map.id == id end
+  #end
 
-  def get(module, id) do
-    Enum.find all(module), fn map -> map.id == id end
-  end
-
-  def get_by(module, params) do
-    Enum.find all(module), fn map ->
-      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end
-  end
+  #def get_by(module, params) do
+    #Enum.find all(module), fn map ->
+      #Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    #end
+  #end
 end
